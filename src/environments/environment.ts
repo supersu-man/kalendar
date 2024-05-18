32e1e889231201ca74bf8841202315aa24c5a95b
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2024-05-18 21:52:27 +0530

    fix: public key and supabase url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    url: "https://scjhaqhrieionmfstdfz.supabase.co",
-    key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjamhhcWhyaWVpb25tZnN0ZGZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ0MDM4MDEsImV4cCI6MjAxOTk3OTgwMX0.QWy--eouUGI6DzNuEUP1JLsm7iTlBSQxvQuOEF8NokI",
-    endpoint: "http://localhost:3000"
-  };
+  url: "https://azrrymdivjzuzqjolvjc.supabase.co",
+  key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6cnJ5bWRpdmp6dXpxam9sdmpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYwNDA1OTIsImV4cCI6MjAzMTYxNjU5Mn0.hQs7xRv8OHWCdmvnEteYq9EV_brJEbfR8qOPtGSpP18",
+  endpoint: "http://localhost:3000"
+};

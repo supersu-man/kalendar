9a4c987af27873bbe0f3f0e5306fecccaaa204e3
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2024-05-27 23:08:13 +0530

    fix: environment.ts and style.css

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   url: "https://azrrymdivjzuzqjolvjc.supabase.co",
   key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6cnJ5bWRpdmp6dXpxam9sdmpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYwNDA1OTIsImV4cCI6MjAzMTYxNjU5Mn0.hQs7xRv8OHWCdmvnEteYq9EV_brJEbfR8qOPtGSpP18",
-  endpoint: "http://localhost:3000"
+  endpoint: "http://localhost:3001"
 };
